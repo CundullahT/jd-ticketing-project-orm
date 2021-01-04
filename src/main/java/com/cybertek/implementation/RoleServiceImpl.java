5383e2918c769cdeb39a98093ec2060e1e901994
CundullahT <cundullahterzioglu@gmail.com>
2021-01-05 00:19:43 +0100

    JD-Ticketing-Application-ORM-Practice

--- src/main/java/com/cybertek/implementation/RoleServiceImpl.java
+++ src/main/java/com/cybertek/implementation/RoleServiceImpl.java
@@ -1,7 +1,10 @@
 package com.cybertek.implementation;
 
 import com.cybertek.dto.RoleDTO;
+import com.cybertek.entity.Role;
+import com.cybertek.repository.RoleRepository;
 import com.cybertek.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,8 +12,16 @@ import java.util.List;
 @Service
 public class RoleServiceImpl implements RoleService {
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @Override
     public List<RoleDTO> listAllRoles() {
+
+        List<Role> roleList = roleRepository.findAll();
+
+        // Convert to DTO and return it (That is why we need Mapper.).
+
         return null;
     }
 
