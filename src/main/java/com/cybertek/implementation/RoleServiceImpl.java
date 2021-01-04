207d460c3e6543c55fd0d6b0fde09c31651d9bbc
CundullahT <cundullahterzioglu@gmail.com>
2021-01-04 23:57:05 +0100

    JD-Ticketing-Application-ORM-Practice

--- src/main/java/com/cybertek/implementation/RoleServiceImpl.java
+++ src/main/java/com/cybertek/implementation/RoleServiceImpl.java
@@ -16,7 +16,7 @@ public class RoleServiceImpl implements RoleService {
     }
 
     @Override
-    public RoleDTO findById(long id) {
+    public RoleDTO findById(Long id) {
         return null;
     }
 
