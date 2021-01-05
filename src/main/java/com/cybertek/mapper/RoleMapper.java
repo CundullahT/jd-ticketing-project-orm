959f8003da31c85688c3f9d29e8bafa5d7bb6572
CundullahT <cundullahterzioglu@gmail.com>
2021-01-05 03:27:35 +0100

    JD-Ticketing-Application-ORM-Practice

---
+++ src/main/java/com/cybertek/mapper/RoleMapper.java
@@ -0,0 +1,23 @@
+package com.cybertek.mapper;
+
+import com.cybertek.dto.RoleDTO;
+import com.cybertek.entity.Role;
+import org.modelmapper.ModelMapper;
+
+public class RoleMapper {
+
+    private ModelMapper modelMapper;
+
+    public RoleMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public Role convertToEntity(RoleDTO dto){
+        return modelMapper.map(dto, Role.class);
+    }
+
+    public RoleDTO convertToDto(Role entity){
+        return modelMapper.map(entity, RoleDTO.class);
+    }
+
+}
