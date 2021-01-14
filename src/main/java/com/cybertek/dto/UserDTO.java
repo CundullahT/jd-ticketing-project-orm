755c34f7da840273eb906d921e936d3dff79e3c6
CundullahT <cundullahterzioglu@gmail.com>
2021-01-14 11:26:57 +0100

    JD-Ticketing-Application-ORM-Practice

--- src/main/java/com/cybertek/dto/UserDTO.java
+++ src/main/java/com/cybertek/dto/UserDTO.java
@@ -12,6 +12,7 @@ import lombok.Setter;
 @Setter
 public class UserDTO {
 
+    private Long id;
     private String firstName;
     private String lastName;
     private String userName;
--- src/main/java/com/cybertek/implementation/ProjectServiceImpl.java
+++ src/main/java/com/cybertek/implementation/ProjectServiceImpl.java
@@ -7,9 +7,11 @@ import com.cybertek.mapper.ProjectMapper;
 import com.cybertek.mapper.UserMapper;
 import com.cybertek.repository.ProjectRepository;
 import com.cybertek.service.ProjectService;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProjectServiceImpl implements ProjectService {
@@ -31,7 +33,7 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public List<ProjectDTO> listAllProjects() {
-        return null;
+        return projectRepository.findAll(Sort.by("projectCode")).stream().map(obj -> {return projectMapper.convertToDto(obj);}).collect(Collectors.toList());
     }
 
     @Override
@@ -39,8 +41,7 @@ public class ProjectServiceImpl implements ProjectService {
         dto.setProjectStatus(Status.OPEN);
         Project obj = projectMapper.convertToEntity(dto);
         obj.setAssignedManager(userMapper.convertToEntity(dto.getAssignedManager()));
-        Project project = projectRepository.save(obj);
-        return project;
+        return projectRepository.save(obj);
     }
 
     @Override
